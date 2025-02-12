78e12d0d76fbcd33f76393fc4b86e4da984af1d2
Ubuntu <ubuntu@ip-172-31-25-184.ec2.internal>
2025-02-12 20:26:39 +0000

    Test push via SSH

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from typing import OrderedDict
 
 from fastapi import APIRouter, status
@@ -60,3 +62,14 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)  # New endpoint
+async def get_book(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Book not found"
+        )
+    return book
